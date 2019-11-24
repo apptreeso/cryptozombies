fe1724cb3c41231e159175ad25806cc34ca3523d
buckdy <buck3dy@gmail.com>
2019-11-24 19:31:12 +1000

    Update asset test animation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <!-- <router-view/>  -->
+    <router-view/> 
   </div> 
 </template>
 
 <script>
-// import AssetTest from '@/views/AssetTest.vue'
 export default {
   name: 'app',
   components: {
-    // AssetTest
   }
 }
 </script>
 
 <style>
 body {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
