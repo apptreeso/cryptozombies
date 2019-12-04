41cf25af274c7f7f3a06738eabe9bcea780ac900
superwebdev19 <superwebdev19@gmail.com>
2019-12-04 12:06:40 +1000

    Implemented 5 QA roughly

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    attemptCount: 0
   },
   mutations: {
+    setAttemptCount(state, payload) {
+      state.attemptCount += payload;
+    }
   },
   actions: {
+    setAttemptCountAction({ commit }, payload) {
+      commit("setAttemptCount", payload);
+    }
   },
-  modules: {
+  getters: {
+    getAttemptCount(state) {
+      return state.attemptCount;
+    }
   }
-})
+});
