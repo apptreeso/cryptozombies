3906f90ac86f9abf294e99e4b6838727e74ecd36
buckdy <buck3dy@gmail.com>
2019-12-11 11:16:52 +0100

    Updated frustration level in QA1

--- src/components/Newbie/QA6.vue
+++ src/components/Newbie/QA6.vue
@@ -123,7 +123,6 @@ export default {
     this.$store.dispatch("setFlagThreeSecondsAction", false);
     this.$store.dispatch("setFlagSkipAction", false);
     this.frustrationLevel = this.$store.state.frustrationLevel;
-    console.log(this.frustrationLevel);
   },
   created: function() {
     this.runTimer(this);
