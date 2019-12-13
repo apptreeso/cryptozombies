c7b618d20b577c98c3a1814fb9994f6e866e6ddd
superwebdev19 <superwebdev19@gmail.com>
2019-12-13 18:11:54 +0100

    Updated QA number and wrong answers

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    flagEnd: false,
+    flagEnd: "none", // true, false, none
     flagEightSeconds: false,
     flagThreeSecods: false,
     flagSkip: false,
